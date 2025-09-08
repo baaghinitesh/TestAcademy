15367d2d47633b2fca59fb9965b65f04e4bf1374
Nitesh Jha <baaghinitesh@gmail.com>
2025-09-09 01:29:14 +0530

    commit 2

--- backend/utils/database.ts
+++ backend/utils/database.ts
@@ -1,6 +1,6 @@
 import mongoose from 'mongoose';
 
-const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://admin:bRZUWoJK@127.0.0.1:27017/lms_db?authSource=admin';
+const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://admin:admin123@127.0.0.1:27017/lms_db?authSource=admin';
 
 if (!MONGODB_URI) {
   throw new Error('Please define the MONGODB_URI environment variable inside .env');
